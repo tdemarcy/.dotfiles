ea34376e37b39743a94a54042d88b7dad459966b
Thomas Demarcy <tdemarcy@gmail.com>
2021-01-15 18:58:04 +0100

    update install.sh

--- .dotfiles/install.sh
+++ .dotfiles/install.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
 
 # Set final keyboard layout
-.keyboard_layout/set_layout.sh
+./keyboard_layout/set_layout.sh
 
 # Install X and i3
 sudo apt -y install xorg
 sudo apt -y install i3
+
+# Install terminal emulator
 sudo apt -y install fonts-hack-ttf
 sudo apt -y install rxvt-unicode
+
+# Install zsh
 sudo apt -y install zsh
+chsh -s $(which zsh)
+sudo ln -s $HOME/.zshrc  /root/.zshrc
+sudo ln -s $HOME/.bash_aliases  /root/.bash_aliases
