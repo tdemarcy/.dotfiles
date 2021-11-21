220ba4523df51f951fbc6da4d8a4c6d3a05d1f7a
tdemarcy <tdemarcy@gmail.com>
2021-11-21 16:11:46 +0100

    Update install.sh

--- .dotfiles/install.sh
+++ .dotfiles/install.sh
@@ -39,6 +39,6 @@ sudo apt -y install python3-tk
 sudo apt -y install feh
 
 # Install Vivaldi
-wget -qO- https://repo.vivaldi.com/archive/linux_signing_key.pub | sudo apt-key add -
-sudo add-apt-repository 'deb https://repo.vivaldi.com/archive/deb/ stable main'
+wget -qO- https://repo.vivaldi.com/archive/linux_signing_key.pub | gpg --dearmor | sudo dd of=/usr/share/keyrings/vivaldi-browser.gpg
+echo "deb [signed-by=/usr/share/keyrings/vivaldi-browser.gpg arch=$(dpkg --print-architecture)] https://repo.vivaldi.com/archive/deb/ stable main" | sudo dd of=/etc/apt/sources.list.d/vivaldi-archive.list
 sudo apt update && sudo apt install vivaldi-stable
