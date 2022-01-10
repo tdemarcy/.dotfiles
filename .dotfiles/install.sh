e7052565cccea6e468289440a322c93301831ac8
Thomas Demarcy <tdemarcy@gmail.com>
2022-01-10 17:47:28 +0100

    add bat

--- .dotfiles/install.sh
+++ .dotfiles/install.sh
@@ -14,22 +14,24 @@ sudo apt -y install rxvt-unicode
 # Install zsh as default shell
 sudo apt -y install zsh
 chsh -s $(which zsh)
-sudo ln -s $HOME/.zshrc /root/.zshrc
-sudo ln -s $HOME/.aliases /root/.aliases
-sudo ln -s $HOME/.dircolors /root/.dircolors
+sudo ln -s ~/.zshrc /root/.zshrc
+sudo ln -s ~/.aliases /root/.aliases
+sudo ln -s ~/.dircolors /root/.dircolors
 sudo rm -f /root/.history
-sudo ln -s $HOME/.history /root/.history
+sudo ln -s ~/.history /root/.history
 sudo chsh -s $(which zsh)
 
 # Set user directories
-mkdir $HOME/downloads
-mkdir $HOME/documents
-mkdir $HOME/music
-mkdir $HOME/pictures
-mkdir $HOME/videos
-mkdir $HOME/projects
+mkdir ~/downloads
+mkdir ~/documents
+mkdir ~/music
+mkdir ~/pictures
+mkdir ~/pictures/screenshots
+mkdir ~/videos
+mkdir ~/projects
 
 # Install command-line tools
+sudo apt -y install bat
 sudo apt -y install curl
 sudo apt -y install fzf
 sudo apt -y install htop
@@ -53,6 +55,11 @@ sudo apt -y install python3-venv
 # Install image viewer
 sudo apt -y install feh
 
+# Install multimedia command line tools
+sudo apt -y install ffmpeg # video
+sudo apt -y install imagemagick # image
+sudo apt -y install poppler-utils # PDF documents
+
 # Install Vivaldi web browser
 wget -qO- https://repo.vivaldi.com/archive/linux_signing_key.pub | gpg --dearmor | sudo dd of=/usr/share/keyrings/vivaldi-browser.gpg
 echo "deb [signed-by=/usr/share/keyrings/vivaldi-browser.gpg arch=$(dpkg --print-architecture)] https://repo.vivaldi.com/archive/deb/ stable main" | sudo dd of=/etc/apt/sources.list.d/vivaldi-archive.list
@@ -70,7 +77,3 @@ sudo apt -y install libreoffice
 # Install Document Viewer
 sudo apt -y install zathura
 xdg-mime default org.pwmt.zathura.desktop application/pdf
-
-# Install multimedia command line tools
-sudo apt -y install ffmpeg
-sudo apt -y install imagemagick
